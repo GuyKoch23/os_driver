dd4fb787fa2a4f713bb35b41da54a3b8b69d97a2
GuyKoch <guykoch@mail.tau.ac.il>
2024-07-05 17:18:23 +0300

    Basic recitation works

---
+++ message_slot.h
@@ -0,0 +1,13 @@
+#ifndef CHARDEV_H
+#define CHARDEV_H
+
+#include <linux/ioctl.h>
+
+#define MAJOR_NUM 235
+#define MSG_SLOT_CHANNEL _IOW(MAJOR_NUM, 0, unsigned long)
+#define DEVICE_RANGE_NAME "message_reader"
+#define BUF_LEN 128
+#define DEVICE_FILE_NAME "simple_message_reader"
+#define SUCCESS 0
+
+#endif
