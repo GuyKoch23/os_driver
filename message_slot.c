a40faeda29f8a0102936e9583d888265b6ed00b9
GuyKoch <guykoch@mail.tau.ac.il>
2024-07-12 15:21:33 +0300

    Feat: atomic read/write, before memory free

--- message_slot.c
+++ message_slot.c
@@ -14,8 +14,6 @@
 #include <linux/uaccess.h>  /* for get_user and put_user */
 #include <linux/string.h>   /* for memset. NOTE - not string.h!*/
 #include <linux/slab.h>
-// #include <errno.h> // handle this
-
 MODULE_LICENSE("GPL");
 
 //Our custom definitions of IOCTL operations
@@ -56,17 +54,18 @@ struct SlotNode{
 struct SlotNode *list_head = NULL;
 //---------------------------------------------------------------
 
-void InsertChannelNode(struct SlotNode *slotNode, int channel){
+int InsertChannelNode(struct SlotNode *slotNode, int channel){
   struct ChannelNode *channelNode = kmalloc(sizeof(struct ChannelNode), GFP_KERNEL);
   if(channelNode == NULL){
     printk(KERN_ERR "Error while trying to allocate channel node");
-    return;
+    return 1;
   }
   channelNode->channel = channel;
   channelNode->current_message_length = 0;
   channelNode->the_message = (char*)kmalloc(sizeof(char)*BUF_LEN, GFP_KERNEL);
   channelNode->next = slotNode->headChannelNode;
   slotNode->headChannelNode = channelNode;
+  return 0;
 };
 
 //---------------------------------------------------------------
@@ -137,6 +136,7 @@ struct SlotNode* get_slot_node(int minor){
 struct ChannelNode* get_channel_node(int minor, int channel){
   struct SlotNode* slotNode = get_slot_node(minor);
   struct ChannelNode *channelNode = slotNode->headChannelNode;
+  int result;
   while(channelNode->channel != 0){
     if(channelNode->channel == channel){
       return channelNode;
@@ -144,7 +144,10 @@ struct ChannelNode* get_channel_node(int minor, int channel){
     channelNode = channelNode->next;
   }
 
-  InsertChannelNode(slotNode, channel);
+  result = InsertChannelNode(slotNode, channel);
+  if(result == 1){
+    return NULL;
+  }
   return slotNode->headChannelNode;
 };
 
@@ -201,30 +204,42 @@ static ssize_t device_read( struct file* file,
     ssize_t i;
     int minor;
     struct ChannelNode *channelNode;
-    if(file->private_data == 0){
-        //errno = EINVAL;
-        return -1;
+    char* tempBuffer;
+    long channel = (unsigned long)(file->private_data);
+    if(channel == 0){
+        return -EINVAL;
     }
-    // if(buffer[0] = NULL){ //  handle this
-    //     //errno = EWOULDBLOCK;
-    //     return -1;
-    // }
-
     minor = iminor(file->f_inode);
 
     channelNode = get_channel_node(minor, (int)(uintptr_t)file->private_data);
-
-    printk("%ld %d", length, channelNode->current_message_length);
+    if(channelNode == NULL){
+      return -ENOMEM;
+    }
+    if(channelNode->current_message_length == 0){
+      return -EWOULDBLOCK;
+    }
     if(length < channelNode->current_message_length){
-        //errno = ENOSPC;
-        return -1;
+      return -ENOSPC;
     }
     //any other error
     printk("Invocing device_read");
-    for(i = 0; i < channelNode->current_message_length && i < BUF_LEN; ++i) {
-        put_user(channelNode->the_message[i], &buffer[i]);
-        printk("%c\n", channelNode->the_message[i]);
+    
+    tempBuffer = (char*)kmalloc(sizeof(char)*channelNode->current_message_length, GFP_KERNEL);
+    if(tempBuffer == NULL){
+      return -ENOMEM;
     }
+
+    for(i = 0; i< channelNode->current_message_length; i++){
+      tempBuffer[i] = channelNode->the_message[i];
+    }
+
+    for(i = 0; i < channelNode->current_message_length && i < BUF_LEN; ++i) {
+        if(put_user(tempBuffer[i], &buffer[i]) != 0){
+          return -EFAULT;
+        }
+    }
+    
+    kfree(tempBuffer);
     printk("%s", buffer);
     return i; // return number of read bytes
 }
@@ -240,24 +255,40 @@ static ssize_t device_write( struct file*       file,
     ssize_t i;
     int minor;
     struct ChannelNode *channelNode;
-    if(file->private_data == 0){
-        //errno = EINVAL;
-        return -1;
+    char* tempBuffer;
+    long channel = (unsigned long)(file->private_data);
+    if(channel == 0){
+        return -EINVAL;
     }
-    if(length == 0 || length > 128){
-        //errno = EMSGSIZE;
-        return -1;
+    if(length == 0 || length > BUF_LEN){
+        return -EMSGSIZE;
     }
     minor = iminor(file->f_inode);
 
     // any other error: -1, errno = what suits
     channelNode = get_channel_node(minor, (int)(uintptr_t)file->private_data);
+      if(channelNode == NULL){
+      return -ENOMEM;
+    }
     printk("Invoking device_write(%p,%ld)\n", file, length);
-    for(i = 0; i < length && i < BUF_LEN; ++i) {
-        get_user(channelNode->the_message[i], &buffer[i]);
+
+    tempBuffer = (char*)kmalloc(sizeof(char)*length, GFP_KERNEL);
+    if(tempBuffer == NULL){
+      return -ENOMEM;
+    }
+
+    for(i = 0; i < length; ++i){
+      if(get_user(tempBuffer[i], &buffer[i]) != 0){
+        return -EFAULT;
+      }
+    }
+
+    for(i = 0; i < length; ++i) {
+         channelNode->the_message[i] = tempBuffer[i];
     }
     printk("message current is %s\n", channelNode->the_message);
     channelNode->current_message_length = i;
+    kfree(tempBuffer);
     return i;
 }
 
@@ -268,8 +299,7 @@ static long device_ioctl( struct   file* file,
 {
   // Switch according to the ioctl called
   if( MSG_SLOT_CHANNEL != ioctl_command_id || ioctl_param == 0) {
-    //errno = EINVAL;
-    return -1;
+    return -EINVAL;
   }
   file->private_data = (void*)ioctl_param;
   return SUCCESS;
@@ -293,35 +323,20 @@ struct file_operations Fops = {
 static int __init simple_init(void)
 {
   int rc = -1;
-  // init dev struct
-  memset( &device_info, 0, sizeof(struct chardev_info) );
+  memset(&device_info, 0, sizeof(struct chardev_info));
   spin_lock_init( &device_info.lock );
-
-  // Register driver capabilities. Obtain major num
-  rc = register_chrdev( MAJOR_NUM, DEVICE_RANGE_NAME, &Fops );
-
-  // Negative values signify an error
+  rc = register_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME, &Fops);
   if( rc < 0 ) {
-    printk( KERN_ERR "registraion failed for  %d\n", MAJOR_NUM );
+    printk(KERN_ERR "registraion failed for  %d\n", MAJOR_NUM);
     return rc;
   }
-
-  printk( "Registeration is successful. ");
-  printk( "If you want to talk to the device driver,\n" );
-  printk( "you have to create a device file:\n" );
-  printk( "mknod /dev/%s c %d 0\n", "device file name", MAJOR_NUM );
-  printk( "You can echo/cat to/from the device file.\n" );
-  printk( "Dont forget to rm the device file and "
-          "rmmod when you're done\n" );
-
+  printk("Registeration is successful.\n");
   return 0;
 }
 
 //---------------------------------------------------------------
 static void __exit simple_cleanup(void)
 {
-  // Unregister the device
-  // Should always succeed
   unregister_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME);
 }
 
